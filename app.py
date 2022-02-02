382f459295b86c19980fa620a85d5c495b09d3dd
Ziyi Zhang <ziyizhang@MacBook-Pro-2.local>
2022-02-02 16:01:19 -0500

    new

---
+++ app.py
@@ -0,0 +1,18 @@
+"""Blogly application."""
+
+from flask import Flask, render_template, request
+from models import db, connect_db, User
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+
+# @app.route("/")
+# def homepage():
+#     return "render_template('home.html')"
